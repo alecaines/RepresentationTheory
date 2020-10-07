6213f8cd7c91e91ab907d62803ec6108bca44d80
M <alecaines@gmail.com>
2020-10-07 17:09:52 -0400

    moving to lab

--- computation/main.py
+++ computation/main.py
@@ -5,8 +5,16 @@ import symmgroup as sg
 #S3 = sg.SYMMETRICGROUP(3)
 #print(S3)
 
+p = perm.PERMUTATION((1,2))
+q = perm.PERMUTATION((2,3))
+pcompq = p.compose(q)
+print(str(p)+str(q), "=", str(pcompq))
+
+
+'''
 p = perm.PERMUTATION((1,3))
 q = perm.PERMUTATION((1,2))
 pcompq = p.compose(q)
 print(str(p)+str(q), "should be (1 2 3)")
 print(str(p)+str(q),"=",str(pcompq))
+'''
